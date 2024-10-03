53f21669e2a61850b527352d88ffbd881c5852c2
funkysys <aadelbos@gmail.com>
2024-10-03 19:07:43 +0200

    added banner on homePage

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -1,23 +1,23 @@
-import prisma from "@/lib/connect";
-import { NextResponse } from "next/server";
+// import prisma from "@/lib/connect";
+// import { NextResponse } from "next/server";
 
-export const POST = async (req: Request) => {
-  try {
-    const body = await req.json();
-    const dataFromBody = {
-      email: body.email,
-    };
-    const post = await prisma.email.create({
-      data: {
-        ...dataFromBody,
-      },
-    });
+// export const POST = async (req: Request) => {
+//   try {
+//     const body = await req.json();
+//     const dataFromBody = {
+//       email: body.email,
+//     };
+//     const post = await prisma.email.create({
+//       data: {
+//         ...dataFromBody,
+//       },
+//     });
 
-    return NextResponse.json(post, { status: 200 });
-  } catch (error) {
-    return NextResponse.json(
-      { error: "Something went wrong" },
-      { status: 500 }
-    );
-  }
-};
+//     return NextResponse.json(post, { status: 200 });
+//   } catch (error) {
+//     return NextResponse.json(
+//       { error: "Something went wrong" },
+//       { status: 500 }
+//     );
+//   }
+// };
