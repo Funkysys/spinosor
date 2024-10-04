81cdac822df14d77a5326429a7073f70da4a758b
Funkysys <aadelbos@gmail.com>
2024-10-04 15:01:55 +0200

    test resolution

--- app/api/auth/[...nextauth]/route.ts
+++
@@ -1,7 +0,0 @@
-import { authOptions } from "@/lib/auth-options";
-import NextAuth from "next-auth/next";
-
-const handler = NextAuth(authOptions);
-console.log("handler", handler);
-
-export { handler as GET, handler as POST };
