7e825cf38e47553bcc5504e11233d5e5ffb811d6
Funkysys <aadelbos@gmail.com>
2024-10-04 15:40:50 +0200

    test again

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,13 @@
 import { authOptions } from "@/lib/auth-options";
 import NextAuth from "next-auth/next";
+import { NextRequest } from "next/server"; // Utilise NextRequest
 
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST };
+export async function GET(req: NextRequest) {
+  return handler(req);
+}
+
+export async function POST(req: NextRequest) {
+  return handler(req);
+}
