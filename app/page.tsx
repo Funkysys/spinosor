86f6ca60458552bab39cf7266707749ac6de7526
Funkysys <aadelbos@gmail.com>
2024-12-19 15:27:07 +0100

    upgrade logo

--- app/page.tsx
+++ app/page.tsx
@@ -6,21 +6,21 @@ import { useRouter } from "next/navigation";
 export default function Home() {
   const router = useRouter();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-perso-bg animate-fade-up animate-once animate-duration-[1500ms] animate-ease-in-out overflow-hidden">
-      <h1 className="mb-10 text-perso-white-one font-bold text-center font-sans text-3xl md:text-4xl lg:text-5xl animate-fade-left animate-once animate-duration-[3000ms] animate-ease-in-out">
+    <main className="flex min-h-screen flex-col items-center justify-center  bg-black animate-fade-up animate-once animate-duration-[1500ms] animate-ease-in-out overflow-hidden">
+      <h1 className=" text-perso-white-one font-bold text-center font-sans text-3xl md:text-4xl lg:text-5xl animate-fade-left animate-once animate-duration-[3000ms] animate-ease-in-out invisible">
         {/* Welcome to Spinosor Records */}
         Bienvenue chez Spinosor Records
       </h1>
-      <button
-        onClick={() => router.push("/home")}
-        className="mb-10 font-sans text-lg bg-perso-white-two hover:bg-perso-yellow-one text-perso-bg hover:text-perso-white-two px-4 py-2 rounded-lg transition duration-400 ease-in-out animate-fade-down animate-once animate-duration-[3000ms] animate-ease-in-out "
-      >
-        {/* Enter the website */}
-        Découvrir le site
-      </button>
-      <div className=" w-[50vh] h-[50vh]  relative animate-fade-up animate-once animate-duration-[3000ms] animate-ease-in-out">
+      <div className=" w-[80vh] h-[80vh]  relative animate-fade-up animate-once animate-duration-[3000ms] animate-ease-in-out">
         <Image src="/assets/images/1.png" alt="Spinosor Records logo" fill />
       </div>
+      <button
+        onClick={() => router.push("/home")}
+        className="font-sans text-2xl bg-perso-white-two hover:bg-perso-yellow-one text-perso-bg hover:text-perso-white-two px-4 py-2 rounded-lg transition duration-400 ease-in-out animate-fade-down animate-once animate-duration-[3000ms] animate-ease-in-out "
+      >
+        {/* Enter the website */}
+        Entrer
+      </button>
     </main>
   );
 }
