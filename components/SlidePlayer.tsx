a7db9034fd660a962670447c4b1b3f6b482a6ae3
Funkysys <aadelbos@gmail.com>
2025-01-06 14:57:49 +0100

    upgrade playlist button and container

--- components/SlidePlayer.tsx
+++ components/SlidePlayer.tsx
@@ -1,6 +1,8 @@
 "use client";
+
 import clsx from "clsx"; // Vous pouvez utiliser clsx pour gérer les classes conditionnelles si vous utilisez Tailwind CSS
 import React, { useState } from "react";
+import { FaMusic } from "react-icons/fa"; // Icône musicale
 
 const SlideModal: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -9,10 +11,11 @@ const SlideModal: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     <div className="absolute h-screen w-screen">
       {/* Icône déclencheur */}
       <div
-        className="fixed top-1/2 left-0 z-10 flex h-12 w-12 cursor-pointer items-center justify-center rounded-full bg-gray-800 text-white transform -translate-y-1/2 hover:bg-gray-600"
+        className="fixed top-1/2 left-0 z-10 flex h-12 w-12 cursor-pointer items-center justify-center rounded-full bg-gray-800 text-white transform-translate-y-1/2 hover:bg-gray-600"
         onMouseEnter={() => setIsOpen(true)}
       >
-        <span className="text-lg">☰</span>
+        <FaMusic className="text-2xl text-perso-yellow-two" />{" "}
+        {/* Nouvelle icône */}
       </div>
 
       {/* Modale */}
@@ -24,7 +27,7 @@ const SlideModal: React.FC<{ children: React.ReactNode }> = ({ children }) => {
         onMouseLeave={() => setIsOpen(false)}
         style={{ minWidth: "10%" }} // Assurez-vous que la largeur minimale est respectée
       >
-        <div className="p-4 flex items-center justify-center h-full ">
+        <div className=" flex items-center justify-center h-full">
           {children}
         </div>
       </div>
