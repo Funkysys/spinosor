f563cff1f6bffb63c20fe8c4b71f1acc9c98e8f6
Funkysys <aadelbos@gmail.com>
2025-01-06 18:03:17 +0100

    upgrade mobile footer

--- components/Login.tsx
+++ components/Login.tsx
@@ -16,7 +16,7 @@ const Login = () => {
       Connection
     </button>
   ) : (
-    <div className="flex h-[10vh] xl:h-[5vh] justify-center items-center text-sm md:mt-0  ">
+    <div className="flex md:h-[10vh] xl:h-[5vh] justify-center items-center text-sm md:mt-0  ">
       {session?.user?.image && (
         <Image
           className="md:w-8 md:h-8 w-8 h-8 mr-3 rounded-full border-2 border-gray-400 "
@@ -29,7 +29,7 @@ const Login = () => {
 
       <button
         onClick={() => signOut({ callbackUrl: "/admin" })}
-        className="flex justify-center items-center mt-3 md:mt-0 right-6 border px-3 py-2 rounded-lg text--600 hover:text-red-800 hover:bg-slate-300  animate-fade-left animate-once animate-duration-[1000ms] animate-ease-in-out"
+        className="flex justify-center items-center right-6 border px-3 py-2 rounded-lg text--600 hover:text-red-800 hover:bg-slate-300  animate-fade-left animate-once animate-duration-[1000ms] animate-ease-in-out"
       >
         Deconnexion
       </button>
