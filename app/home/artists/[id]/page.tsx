c0a93617877d67f994207a83e16123ea2e7ec986
funkysys <aadelbos@gmail.com>
2025-01-25 23:34:43 +0100

    upgrade backoffice artistCreation

--- app/home/artists/[id]/page.tsx
+++ app/home/artists/[id]/page.tsx
@@ -100,7 +100,7 @@ const ArtistPage = () => {
     <div className="w-[100vw] md:w-[66vw] min-h-[100vh] mx-auto px-4 md:px-8  bg-perso-bg2 border-x-2 border-sky-950">
       <button
         onClick={goToNextArtist}
-        className="md:fixed text-sm mb-5 top-48 md:right-5 lg:right-10 xl:right-52 font-ruda bg-perso-yellow-one text-perso-bg px-4 py-2 rounded-lg hover:bg-blue-800 hover:text-perso-white-two transition"
+        className="fixed z-50 text-sm right-4 top-24 md:top-32 md:right-8 lg:right-12 xl:right-48 font-ruda bg-perso-yellow-one text-perso-bg px-4 py-2 rounded-lg hover:bg-blue-800 hover:text-perso-white-two transition shadow-md"
       >
         Artiste suivant
       </button>
