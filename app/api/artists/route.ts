ab90941a44c889f78ed2aabcd0ad6b99ad5ea1db
funkysys <aadelbos@gmail.com>
2025-03-19 19:09:17 +0100

    MAJ API ROUTES FOR GET METHOD

--- app/api/artists/route.ts
+++ app/api/artists/route.ts
@@ -1,7 +1,7 @@
 import prisma from "@/lib/connect";
 import { NextResponse } from "next/server";
 
-export const GET_ARTISTS = async () => {
+export const GET = async () => {
   try {
     const artists = await prisma.artist.findMany({
       include: {
